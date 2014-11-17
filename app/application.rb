f45fb004ec84fd61e41e1647d73c9ae90411eb6a
Will Mitchell <wvmitchell@gmail.com>
2014-11-17 13:53:28 -0700

    using HTTP get with params

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,38 @@ Document.ready? do
 
   box = Element.find('#red_box')
 
-  box.on(:mouseover) do
-    width = get_window_width
-    height = get_window_height
-    top = positive_in_range(height)
-    left =  positive_in_range(width)
-    box.animate(top: "#{top}px", left: "#{left}px", speed: 500)
+  box.on(:click) do
+    adjust_size(box)
+    adjust_position(box)
+    adjust_color(box)
   end
 
+  Element['#test_params'].on(:click) do
+    HTTP.get('/test_params', data: {name: 'Will'}) do |response|
+      alert JSON.parse(response.body)['alert']
+    end
+  end
+
+end
+
+def adjust_size(element)
+  width = rand(20..200)
+  height = rand(20..200)
+  element.animate(width: width, height: height, speed: 500)
+end
+
+def adjust_color(element)
+  HTTP.get('/colors.json') do |response|
+    colors = JSON.parse(response.body)['colors']
+    color = colors[rand(colors.count - 1)]
+    element.css('background-color', color)
+  end
+end
+
+def adjust_position(element)
+  #width = get_window_width
+  #height = get_window_height
+  #element.animate(top: "#{top}px", left: "#{left}px", speed: 500)
 end
 
 def get_window_width
--- controllers/main.rb
+++ controllers/main.rb
@@ -1,3 +1,18 @@
 get '/' do
   haml :index
 end
+
+get '/colors.json' do
+  { colors: ['yellow',
+             'green',
+             'blue',
+             'red',
+             'black',
+             'purple'] }.to_json
+end
+
+get '/test_params' do
+  binding.pry
+  person = params["name"]
+  { alert: "Here\'s an alert #{person}!" }.to_json
+end
